8a9031a5f9744883602a4c1f09c58f28e93d076f
weirenw <weirenwang@gatech.edu>
2016-02-04 16:21:08 -0500

    fill passwd done

--- Chrome_Extension/content.js
+++ Chrome_Extension/content.js
@@ -2,11 +2,12 @@
 var editorExtensionId = "fdgaobbifkkcaebeghfhnbdaljmnfjnj";
 var url = document.location.href;
 // Make a simple request:
-
+var passwordField = document.getElementById('Passwd');
 console.log("sending  "+url);  
 console.log("sending to "+editorExtensionId);
 chrome.runtime.sendMessage(editorExtensionId, {openUrlInEditor: url},function(response) {
   console.log("response: "+JSON.stringify(response));
+  passwordField.value = JSON.stringify(response);
 });
 
 /*
