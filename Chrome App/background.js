acff296f543bab0860605a10198249a5e1087510
Weiren Wang <weiren@weiren.gtisc.gatech.edu>
2016-02-04 20:07:51 -0500

    test on laptop

--- Chrome App/background.js
+++ Chrome App/background.js
@@ -1,9 +1,10 @@
 console.log("chrome app background js");
+/*
 chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
   	console.log("in listenr");
 	if (request.openUrlInEditor){
 		console.log("from "+sender.id+": "+request.openUrlInEditor);
-		sendResponse({"password123"});
+		sendResponse({"result":"gtiisp123"});
 	}else if(request.myCustomMessage){
 		console.log("from "+sender.id+": "+request.myCustomMessage);
 		sendResponse({"result":"Ok, got your message"});
@@ -12,8 +13,8 @@ chrome.runtime.onMessageExternal.addListener(function(request, sender, sendRespo
 	  sendResponse({"result":"Ops, I don't understand this message"});
 	}
 });
+*/
 
-/*
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
     'outerBounds': {
@@ -22,4 +23,3 @@ chrome.app.runtime.onLaunched.addListener(function() {
     }
   });
 });
-*/
--- Chrome App/listener.js
+++ Chrome App/listener.js
@@ -1,11 +1,16 @@
 chrome.runtime.onMessageExternal.addListener(
   function(request, sender, sendResponse) {
     console.log("Request", request);
-    send_bluetooth(socketId, request); // From bluetooth.js
+    if(request.openUrlInEditor){
+    console.log("from "+sender.id+": "+request.openUrlInEditor);
+    send_bluetooth(socketId, request.openUrlInEditor); // From bluetooth.js
     setTimeout(function() {
       response = read_latest_message();
       console.log("Response",response);
-      sendResponse(response);
+      sendResponse("result" : response);
     }, 1000);
+    }else{
+	sendResponse({"result":"Ops, I don't understand this message"});
+    }
     return true;
 });
