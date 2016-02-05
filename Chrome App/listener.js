66b2d66d9cb426c996e2041b35729d856e3d6589
weirenw <weirenwang@gatech.edu>
2016-02-04 20:27:29 -0500

    ready to integrate

--- Chrome App/listener.js
+++ Chrome App/listener.js
@@ -3,11 +3,12 @@ chrome.runtime.onMessageExternal.addListener(
     console.log("Request", request);
     if(request.openUrlInEditor){
     console.log("from "+sender.id+": "+request.openUrlInEditor);
+//    sendResponse({"result" : "gtiisp123"});
     send_bluetooth(socketId, request.openUrlInEditor); // From bluetooth.js
     setTimeout(function() {
       response = read_latest_message();
       console.log("Response",response);
-      sendResponse("result" : response);
+      sendResponse( response);
     }, 1000);
     }else{
 	sendResponse({"result":"Ops, I don't understand this message"});
